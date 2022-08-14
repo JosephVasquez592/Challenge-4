e509e61de1ac1e219d2d220f1c4fed0170d8439e
Joey <107812238+JosephVasquez592@users.noreply.github.com>
2022-08-14 11:20:56 -0600

    added several lines of code

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        q: 'What is HTML?',
+        c: ['Hypertext Markup Language', 'Hyperlink Markup Language', 'Hidden Text Media Library', 'Hot Tamale Mother Load'],
+        a: 'Hypertext Markup Language',
+    },
+    {
+        q: 'What is CSS?',
+        c: ['Color Styling Sheet', 'Correspondence Styling Service', 'Cascading Style Sheet', 'Cool Super Soaker'],
+        a: 'Cascading Style Sheet',
+    },
+    {
+        q: 'What is JS?',
+        c: ['JumpStart', 'JavaScript', 'Jalapeño Sauce', 'JuiceScript'],
+        a: 'JavaScript',
+    },
+    {
+        q: 'What do HTML, CSS, and JS have in common?',
+        c: ['They are all acronyms', 'They are all used for designing webpages', 'They all have specific rules', 'All of the above'],
+        a: 'All of the above',
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+var questionNumber = 0;
+var timer = questions.length * 20;
+var startTimer;
